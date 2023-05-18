23595590b0f795678beedf9eb6516be0305a9574
Parth4120 <122157202+Parth4120@users.noreply.github.com>
2023-05-18 19:26:40 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,16 @@
+
+// function greet(name,greettex="good man"){
+//     console.log(greettex+" "+name)
+//     console.log(name+"this is function ");
+// }
+// let name="manan";
+// let name1="jash";
+// let name2="dev";
+// let name3="raj";
+// let greettex="good morning"
+// greet(name1,greettex);
+// greet(name2)
+// greet(name3)
+
+
+
